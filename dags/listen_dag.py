8d4721a4dd4c3f911f6e75d5274b8145b7988d66
aswinganga-greeninkpro <aswinganga.greeninkpro@gmail.com>
2025-08-18 23:55:55 +0530

    change/ dag added

---
+++ dags/listen_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+
+def check_xcom(**context):
+    pod_info = context['ti'].xcom_pull(task_ids="run_transaction_tally")
+    print("==== XCom Pod Info ====")
+    print(pod_info)  # will also show in Airflow logs
+    return pod_info   # makes it visible in the XCom tab
+
+with DAG(
+    dag_id="transaction_tally_dag",
+    schedule_interval=None,
+    start_date=days_ago(1),
+    catchup=False,
+) as dag:
+
+    run_python_app = KubernetesPodOperator(
+        task_id="run_transaction_tally",
+        name="transaction-tally",
+        namespace="default",
+        image="ghcr.io/vishnu-thirumangalath/docker-images/tansaction-tally:latest",
+        cmds=["python", "run.py"],
+        get_logs=True,
+        do_xcom_push=True,   # capture pod info
+    )
+
+    check_pod_xcom = PythonOperator(
+        task_id="check_pod_xcom",
+        python_callable=check_xcom,
+    )
+
+    run_python_app >> check_pod_xcom
