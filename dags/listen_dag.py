3bdd69cd8b56818dd9b00542184b5df23a801580
aswinganga-greeninkpro <aswinganga.greeninkpro@gmail.com>
2025-08-22 20:20:53 +0530

    restored dag

--- dags/listen_dag.py
+++ dags/listen_dag.py
@@ -14,7 +14,7 @@ with DAG(
     # 1. Run Flask app pod (kept alive, task ends immediately after creation)
     run_python_app = KubernetesPodOperator(
         task_id="run_transaction_tally",
-        name="transaction-tally-{{ ds-nodash }}",
+        name="transaction-tally",
         namespace="test",
         service_account_name="dagsvc",
         image="ghcr.io/vishnu-thirumangalath/docker-images/transaction-tally:latest",
